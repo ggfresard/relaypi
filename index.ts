32d26a735e3c299930b6e85ca4b4af5b72ab7d50
Gabriel Pablo González Fresard <legab@Gabriels-MacBook-Pro.local>
2023-02-10 17:28:09 -0300

    dd

--- index.ts
+++ index.ts
@@ -7,30 +7,43 @@ var state: 0 | 1 = 0
 
 relay.writeSync(state)
 
-const socket = io("https://masterapi.legab.ninja")
+var checker = setInterval(async () => {
+    if (
+        !!(await require("dns")
+            .promises.resolve("google.com")
+            .catch(() => {}))
+    ) {
+        init()
+        clearInterval(checker)
+    }
+}, 1000)
 
-const update = () => {
-    relay.writeSync(state)
+const init = async () => {
+    const socket = io("https://masterapi.legab.ninja")
+
+    const update = () => {
+        relay.writeSync(state)
+    }
+    socket.on("connect", () => {
+        console.log("connected")
+
+        socket.on("request_state", () => {
+            socket.emit("return_state", state)
+        })
+
+        socket.on("switch", (data) => {
+            switch (data) {
+                case "on":
+                    state = 1
+
+                    console.log("on")
+                    break
+                case "off":
+                    state = 0
+                    console.log("off")
+                    break
+            }
+            update()
+        })
+    })
 }
-socket.on("connect", () => {
-    console.log("connected")
-
-    socket.on("request_state", () => {
-        socket.emit("return_state", state)
-    })
-
-    socket.on("switch", (data) => {
-        switch (data) {
-            case "on":
-                state = 1
-
-                console.log("on")
-                break
-            case "off":
-                state = 0
-                console.log("off")
-                break
-        }
-        update()
-    })
-})
