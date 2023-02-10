91fc2775d7b1f048e405ffa5947a7a568683e89f
Gabriel Pablo González Fresard <legab@Gabriels-MacBook-Pro.local>
2023-02-10 16:31:00 -0300

    ss

--- index.ts
+++ index.ts
@@ -3,26 +3,34 @@ import { io } from "socket.io-client"
 
 var relay = new Gpio(21, "high")
 
-relay.writeSync(0)
+var state: 0 | 1 = 0
+
+relay.writeSync(state)
 
 const socket = io("https://masterapi.legab.ninja")
 
+const update = () => {
+    relay.writeSync(state)
+}
 socket.on("connect", () => {
     console.log("connected")
-    socket.on("message", () => {
-        console.log("message")
+
+    socket.on("request_state", () => {
+        socket.emit("return_state", state)
     })
 
     socket.on("switch", (data) => {
         switch (data) {
             case "on":
-                relay.writeSync(1)
+                state = 1
+
                 console.log("on")
                 break
             case "off":
-                relay.writeSync(0)
+                state = 0
                 console.log("off")
                 break
         }
+        update()
     })
 })
